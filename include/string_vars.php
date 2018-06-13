50ef7f9a791b87feb79a11481a7993b4fd2f2c7c
nstrucky <nickstruckmeyer@gmail.com>
2018-06-12 22:05:56 -0400

    Added PassHash file and notes

--- include/string_vars.php
+++ include/string_vars.php
@@ -8,13 +8,15 @@
 /**
  * Database configuration
  */
-define("DB_SERVER", 'localhost');
-define("DB_DATABASE", 'task_manager');
-define("DB_USER", 'root');
-define("DB_PASSWORD", '');
-define("DB_SERVER_PORT", '3306');
+define('DB_SERVER', 'localhost');
+define('DB_DATABASE', 'task_manager');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_SERVER_PORT', '3306');
 
-
+define('USER_CREATED_SUCCESSFULLY', 0);
+define('USER_CREATE_FAILED', 1);
+define('USER_ALREADY_EXISTED', 2);
 
 
 ?>

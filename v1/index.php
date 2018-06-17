eb770234581ed7947b1061663c2ade31ed2c32e0
nstrucky <nickstruckmeyer@gmail.com>
2018-06-17 16:01:25 -0400

    replaced slim lib
    originally included the 3.x framework version, however the tutorial is using the 2.x version.  Also added the verifyRequiredParams method to index.php

--- v1/index.php
+++ v1/index.php
@@ -1,17 +1,50 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+
+ require_once '../include/DbHandler.php';
+ require_once '../include/PassHash.php';
+ require_once '.././libs/Slim/Slim.php';
+
+  \Slim\Slim::registerAutoLoader();
+  
+  $app = new \Slim\Slim();
+  
+  //User id from db - Global Variable
+  $user_id = NULL;
+  
+  function verifyRequiredParams($required_fields) {
+      
+      $error = FALSE;
+      $error_fields = "";
+      
+      //why do we assign to this variable twice?
+      $request_params = array();
+      $request_params = $_REQUEST;
+      
+      if ($_SERVER['REQUEST_METHOD'] == 'PUT') { // really shouldn't do it this way
+          $app = \Slim\Slim::getInstance(); //why getInstance here when it's already instantiated above?
+          parse_str($app->request()->getBody(), $request_params);
+          
+      }
+      
+      foreach($required_fields as $field) {
+          if (!isset($request_params[$field]) || 
+                  strlen($request_params[$field]) <= 0) {
+              $error = TRUE;
+              $error_fields .= $field . ', ';
+          }
+      }
+      
+      if ($error) {
+          $response = array();
+          $app = \Slim\Slim::getInstance();
+          $response["error"] = true;
+          $response["message"] = 'Required field(s) ' . 
+                  substr($error_fields,0, -2) . ' is missing or empty.';
+          echoResponse(400, $response);
+          $app->stop();
+      }
+  }
+  
+  
+
+?>
